0717fbaf15b1360afc7df0435fb88c2aadd0d9b5
Arjun Dobaria <arjun.dobaria12@gmail.com>
2019-02-19 19:40:08 +0530

    Changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var smtpTransport = nodemailer.createTransport({
         pass: "Krishna143"
     }
 });
-var rand,mailOptions,host,link;
+var rand, mailOptions, host, link;
 
 //--------------------------------------------------------------------------------------------------------------
 //COLLECTIONS
@@ -138,7 +138,8 @@ client.connect((err, db) => {
 
             var dataArray = dbo.collection(switlover).find({
                 'Phone_Number.Number': req.body.Number,
-                'Phone_Number.Contry_Code': req.body.Contry_Code
+                'Phone_Number.Contry_Code': req.body.Contry_Code,
+                is_Block: {$ne: "1"}
             }).toArray();
             dataArray.then((data) => {
 
@@ -157,6 +158,7 @@ client.connect((err, db) => {
                                     Location: req.body.Location,
                                     Verified: "false"
                                 },
+                                Like: {Likes: []},
                                 Email: "",
                                 Contact_List: "",
                                 PowerID: {Power_Of_Match: "0", Power_Of_Time: "0", Golden_Power: "0"},
@@ -245,7 +247,8 @@ client.connect((err, db) => {
             var Phone_Number = req.body.Contry_Code + "" + req.body.Number;
             var dataArray = dbo.collection(switlover).find({
                 'Phone_Number.Number': req.body.Number,
-                'Phone_Number.Contry_Code': req.body.Contry_Code
+                'Phone_Number.Contry_Code': req.body.Contry_Code,
+                isBlock: {$ne: "1"}
             }).toArray();
             dataArray.then((data) => {
                 if (isEmpty(data))
@@ -285,30 +288,28 @@ client.connect((err, db) => {
             }).catch((err) => {
                 res.json({status: "1", message: "Internal Server error"});
             });
-        })
+        });
         //--------------------------------------------------------------------------------------------------------------
-
-
 
         //--------------------------------------------------------------------------------------------------------------
         //Send Email For Verification
-        app.get('/api/EmailVerification',(req,res) => {
-            rand=Math.floor((Math.random() * 1000) + 54);
-            host=req.get('host');
-            link="http://"+req.get('host')+"/verify?id="+rand;
-            mailOptions={
-                to : req.query.Email,
-                subject : "Please confirm your Email account",
-                html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+        app.get('/api/EmailVerification', (req, res) => {
+            rand = Math.floor((Math.random() * 1000) + 54);
+            host = req.get('host');
+            link = "http://" + req.get('host') + "/verify?id=" + rand;
+            mailOptions = {
+                to: req.query.Email,
+                subject: "Please confirm your Email account",
+                html: "Hello,<br> Please Click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
             }
             console.log(mailOptions);
-            smtpTransport.sendMail(mailOptions, function(error, response){
-                if(error){
+            smtpTransport.sendMail(mailOptions, function (error, response) {
+                if (error) {
                     console.log(error);
-                    res.end("error"+error);
-                }else{
+                    res.end("error" + error);
+                } else {
                     console.log("Message sent: " + response);
-                    res.end("sent"+response);
+                    res.end("sent" + response);
                 }
             });
         });
@@ -316,29 +317,22 @@ client.connect((err, db) => {
 
         //--------------------------------------------------------------------------------------------------------------
         //Email Verification
-        app.get('/verify',function(req,res){
-            console.log(req.protocol+":/"+req.get('host'));
-            if((req.protocol+"://"+req.get('host'))==("http://"+host))
-            {
+        app.get('/verify', function (req, res) {
+            console.log(req.protocol + ":/" + req.get('host'));
+            if ((req.protocol + "://" + req.get('host')) == ("http://" + host)) {
                 console.log("Domain is matched. Information is from Authentic email");
-                if(req.query.id==rand)
-                {
+                if (req.query.id == rand) {
                     console.log("email is verified");
-                    res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
-                }
-                else
-                {
+                    res.end("<h1>Email " + mailOptions.to + " is been Successfully verified");
+                } else {
                     console.log("email is not verified");
                     res.end("<h1>Bad Request</h1>");
                 }
-            }
-            else
-            {
+            } else {
                 res.end("<h1>Request is from unknown source");
             }
         });
         //--------------------------------------------------------------------------------------------------------------
-
 
 
     }
