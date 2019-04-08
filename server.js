1837fc9520ca115e1285d9cdd598af9332dc99c7
Arjun Dobaria <arjun.dobaria12@gmail.com>
2019-04-08 17:37:44 +0530

    change

--- server.js
+++ server.js
@@ -1527,7 +1527,7 @@ client.connect((err, db) => {
                 var Auth_Token = req.header('Auth_Token');
                 console.log("/api/SetNotificationSettings")
                 console.log("Body" + req.body);
-                console.log("Body" + req.body.data["userID"]);
+                console.log("Body" + req.body.data);
                 if (!Auth_Token || Auth_Token == null) {
                     res.json({status: "6", message: "Auth token missing"});
                 } else {
